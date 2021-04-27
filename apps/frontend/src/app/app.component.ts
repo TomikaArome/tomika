798769b39f3dd9cf4d58aef77136e29ed63bdb67
Thomas Hunter <tommychasseur@gmail.com>
2021-04-27 12:36:05 +0100

    [TC-2] Add permission model

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@tomika/api-interfaces';
 
 @Component({
   selector: 'tomika-root',
@@ -8,6 +7,6 @@ import { Message } from '@tomika/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  hello$ = this.http.get('/api/hello');
   constructor(private http: HttpClient) {}
 }

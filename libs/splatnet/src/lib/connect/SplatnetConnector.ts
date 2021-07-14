2dc20ed45ff1fc1767d098ee3c076e796a175d2f
Thomas Hunter <tommychasseur@gmail.com>
2021-07-14 16:52:14 +0100

    [TC-4] Started implementing some logic

--- libs/splatnet/src/lib/connect/SplatnetConnector.ts
+++ libs/splatnet/src/lib/connect/SplatnetConnector.ts
@@ -1,3 +1,6 @@
+import { randomBytes, createHash } from 'crypto';
+import { uuidv4 } from 'uuid';
+
 type SplatnetConnectorOptions = {
   cookie: string;
   sessionTokenCode: never;
@@ -11,6 +14,7 @@ export class SplatnetConnector {
 
   private static readonly NINTENDO_SWITCH_ONLINE_CLIENT_ID = '71b963c1b7b6d119';
   private static readonly SESSION_TOKEN_URL = 'https://accounts.nintendo.com/connect/1.0.0/api/session_token';
+  private static readonly AUTHORIZE_URL = 'https://accounts.nintendo.com/connect/1.0.0/authorize';
 
   constructor(options: SplatnetConnectorOptions) {
     this.cookie = options.cookie ?? null;
@@ -19,4 +23,27 @@ export class SplatnetConnector {
   private getSessionToken(): string {
     return '';
   }
+
+  static generateAuthUrl(authCodeVerifier: string) {
+    const state = SplatnetConnector.toUrlSafeBase64Encode(randomBytes(36));
+
+    const authCvHash = createHash('sha256');
+    authCvHash.update(authCodeVerifier);
+    const authCodeChallenge = SplatnetConnector.toUrlSafeBase64Encode(authCvHash.digest());
+  }
+
+  static generateAuthCodeVerifier(): string {
+    return SplatnetConnector.toUrlSafeBase64Encode(randomBytes(32));
+  }
+
+  private static toUrlSafeBase64Encode(val): string {
+    return val.toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=+$/, '');
+  };
+
+  private static buildQuery = (parameters: { [key: string]: string }) => {
+    return Object.entries(parameters).map(([key, value]) => `${key}=${value}`).join('&');
+  };
 }

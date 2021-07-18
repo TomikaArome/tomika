12e61fa20ab086ec756657fc84c4117550857e21
Thomas Hunter <tommychasseur@gmail.com>
2021-07-18 15:46:32 +0100

    [6] General angular frontend app setup (#4)

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { IndexComponent } from '../index/index.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/frontend/src/index/index.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tmk-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss'],
+})
+export class IndexComponent {}

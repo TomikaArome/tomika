13d1e3c1feeda43ae14a011f06528338b84716d3
Thomas Hunter <tommychasseur@gmail.com>
2021-07-19 00:32:20 +0100

    [7] Added admin panel and rudimentary permissions tree

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from '../index/index.component';
+import { AdminPanelComponent } from '../admin/panel/admin-panel.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent }
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: 'admin',
+    component: AdminPanelComponent
+  }
 ];
 
 @NgModule({

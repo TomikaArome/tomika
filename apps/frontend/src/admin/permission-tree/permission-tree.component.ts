1dd4ec54edbf46fccf4fa4efdb676be1f5563c5a
Thomas Hunter <tommychasseur@gmail.com>
2021-07-19 12:36:57 +0100

    [7] Changed to flat tree

--- apps/frontend/src/admin/permission-tree/permission-tree.component.ts
+++ apps/frontend/src/admin/permission-tree/permission-tree.component.ts
@@ -1,10 +1,17 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { NestedTreeControl } from '@angular/cdk/tree';
+import { Component } from '@angular/core';
+import { FlatTreeControl } from '@angular/cdk/tree';
 import { Permission } from '@TomikaArome/common';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
 import { permissionTreeMock } from '../../../../../libs/common/src/mock/user/permission.mock';
 
 const TREE_DATA = permissionTreeMock;
+
+interface FlatPermissionNode {
+  expandable: boolean;
+  level: number;
+  permission: Permission;
+  label: string;
+}
 
 @Component({
   selector: 'tmk-permission-tree',
@@ -12,19 +19,36 @@ const TREE_DATA = permissionTreeMock;
   styleUrls: ['./permission-tree.component.scss']
 })
 export class PermissionTreeComponent {
-  treeControl = new NestedTreeControl<Permission>(permission => permission.children);
-  dataSource = new MatTreeNestedDataSource<Permission>();
+  treeControl: FlatTreeControl<FlatPermissionNode>;
+  treeFlattener: MatTreeFlattener<Permission, FlatPermissionNode>;
+  dataSource: MatTreeFlatDataSource<Permission, FlatPermissionNode>;
 
   constructor() {
+    this.treeControl = new FlatTreeControl<FlatPermissionNode>(node => node.level, node => node.expandable);
+    this.treeFlattener = new MatTreeFlattener<Permission, FlatPermissionNode>(
+      this.flattenNode,
+      node => node.level,
+      node => node.expandable,
+      permission => permission.children);
+    this.dataSource = new MatTreeFlatDataSource<Permission, FlatPermissionNode>(this.treeControl, this.treeFlattener);
     this.dataSource.data = [TREE_DATA];
-    this.treeControl.expand(TREE_DATA);
+    this.treeControl.expand(this.treeControl.dataNodes[0]);
   }
 
-  permissionHasChildren = (_: number, permission: Permission): boolean => {
-    return !!permission.children && permission.children.length > 0;
+  flattenNode = (node: Permission, level: number): FlatPermissionNode => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      permission: node,
+      level: level,
+      get label() { return node.label; }
+    }
   }
 
-  isRootPermission(permission: Permission): boolean {
-    return permission === TREE_DATA;
+  showExpandButton(node: FlatPermissionNode): boolean {
+    return node.expandable && node.permission !== TREE_DATA;
+  }
+
+  nodeIndent(node: FlatPermissionNode) {
+    return `calc(${Math.max(0, node.level - 1) * 24 + (node.expandable ? 0 : 28)}px + 1em)`;
   }
 }

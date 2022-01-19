fc92d2753712a773bad0ab2708bc908020bbd30f
Thomas Hunter <tommychasseur@gmail.com>
2022-01-19 00:13:06 +0000

    Finished positioning the cards

--- libs/ouistiti-ui/src/lib/components/game-flow/card-container/card-container.component.ts
+++ libs/ouistiti-ui/src/lib/components/game-flow/card-container/card-container.component.ts
@@ -1,18 +1,23 @@
 import { Component, ElementRef, HostListener, Input, OnInit } from '@angular/core';
-import { BidInfo, CARD_ORDER, CardInfo, CardSuit, KnownCardInfo, OwnedAndKnownCardInfo, OwnedAndUnknownCardInfo, PlayedCardInfo, PlayerInfo, RoundInfo, RoundStatus, WonCardInfo } from '@TomikaArome/ouistiti-shared';
+import { BidInfo, CARD_ORDER, CardInfo, CardSuit, KnownCardInfo, OwnedAndKnownCardInfo, OwnedAndUnknownCardInfo, PlayedCardInfo, PlayerInfo, RoundInfo, RoundStatus, TrumpCardInfo, WonCardInfo } from '@TomikaArome/ouistiti-shared';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
 
 interface PlayerRoundDetailsPosition {
   x: number;
   y: number;
   angle: number;
+  width?: number;
+  height?: number;
 }
 
 interface CardPosition {
   x?: number;
   y?: number;
   z?: number;
+  width?: number;
   height?: number;
   rotation?: number;
+  playable?: boolean;
 }
 
 @Component({
@@ -31,6 +36,8 @@ export class CardContainerComponent implements OnInit {
   private containerWidth: number;
   private containerHeight: number;
 
+  faBan = faBan;
+
   get cards() {
     return this.roundInfo.cards ?? [];
   }
@@ -42,6 +49,26 @@ export class CardContainerComponent implements OnInit {
 
   get areBidsStillPending(): boolean {
     return this.roundInfo?.status === RoundStatus.BIDDING;
+  }
+
+  get trumpCardBorderStyle() {
+    const cardPos = this.getUnownedCardPosition();
+    return {
+      transform: `translate(${cardPos.x}px,${cardPos.y}px)`
+    };
+  }
+
+  get trumpCardBorderContentsStyle() {
+    const cardPos = this.getUnownedCardPosition();
+    return {
+      width: `${cardPos.width + 12}px`,
+      height: `${cardPos.height + 12}px`,
+      opacity: this.hasTrumps ? 0 : 1
+    }
+  }
+
+  get hasTrumps(): boolean {
+    return this.roundInfo.cards.findIndex((c: CardInfo) => (c as TrumpCardInfo).isTrumpCard) > -1;
   }
 
   private static toRadians(degrees: number): number {
@@ -88,13 +115,15 @@ export class CardContainerComponent implements OnInit {
   getPlayerRoundDetailsPosition(playerId: string): PlayerRoundDetailsPosition {
     const players = this.playersWithoutSelf.map((player: PlayerInfo) => player.id);
 
-    let rx, ry;
+    let rx, ry, width, height;
     const el = ((this.elRef.nativeElement as HTMLElement)
       .querySelector('#player-round-details-' + playerId) as HTMLElement);
     if (el) {
       const box = el.getBoundingClientRect();
       rx = this.containerWidth - box.width / 2;
       ry = this.containerHeight - box.height / 2;
+      width = box.width;
+      height = box.height;
     }
 
     if (playerId === this.selfId) {
@@ -111,17 +140,25 @@ export class CardContainerComponent implements OnInit {
       return {
         x: Math.cos(CardContainerComponent.toRadians(angle - 90)) * rx,
         y: Math.sin(CardContainerComponent.toRadians(angle - 90)) * ry,
-        angle
+        angle,
+        width,
+        height
       }
     }
   }
 
   getCardPosition(card: CardInfo): CardPosition {
-    let cardPos: CardPosition = {};
+    let cardPos: CardPosition;
 
-    if ((card as OwnedAndKnownCardInfo).ownerId === this.selfId) {
+    if ((card as OwnedAndUnknownCardInfo).ownerId === undefined) {
+      cardPos = this.getUnownedCardPosition();
+    } else if ((card as WonCardInfo).winnerId !== undefined) {
+      cardPos = this.getWonCardPosition(card as WonCardInfo);
+    } else if ((card as PlayedCardInfo).playedOnTurn !== undefined) {
+      cardPos = this.getPlayedCardPosition(card as PlayedCardInfo);
+    } else if ((card as OwnedAndKnownCardInfo).ownerId === this.selfId) {
       cardPos = this.getCardPositionInOwnHand(card as OwnedAndKnownCardInfo);
-    } else if ((card as OwnedAndUnknownCardInfo).ownerId !== undefined) {
+    } else {
       cardPos = this.getCardPositionInOtherHand(card as OwnedAndUnknownCardInfo);
     }
 
@@ -129,8 +166,10 @@ export class CardContainerComponent implements OnInit {
       x: 0,
       y: 0,
       z: 0,
+      width: 125,
       height: 175,
       rotation: 0,
+      playable: false,
       ...cardPos
     }
   }
@@ -144,6 +183,14 @@ export class CardContainerComponent implements OnInit {
       }
       return diff;
     })
+  }
+
+  isCardPlayable(card: OwnedAndKnownCardInfo, cardsInHand: OwnedAndKnownCardInfo[]): boolean {
+    if (this.roundInfo.status !== RoundStatus.PLAY || this.roundInfo.currentPlayerId !== card.ownerId) { return false; }
+    const leadingCard = this.roundInfo.cards.find((c: PlayedCardInfo) =>
+      c.playedOnTurn === this.roundInfo.currentTurnNumber && c.playedOrderPosition === 1) as PlayedCardInfo;
+    if (!leadingCard || card.suit === leadingCard.suit) { return true; }
+    return cardsInHand.findIndex((c: OwnedAndKnownCardInfo) => c.suit === leadingCard.suit) === -1;
   }
 
   getCardPositionInOwnHand(card: OwnedAndKnownCardInfo): CardPosition {
@@ -163,13 +210,68 @@ export class CardContainerComponent implements OnInit {
       x: Math.cos(CardContainerComponent.toRadians(angle - 90)) * radius,
       y: this.containerHeight - (Math.sin(CardContainerComponent.toRadians(angle + 90)) - Math.sin(CardContainerComponent.toRadians(angleOfFirstCard + 90))) * radius - 125,
       z: cardsInHand.indexOf(card),
-      rotation: angle
+      rotation: angle,
+      playable: this.isCardPlayable(card, cardsInHand)
     }
   }
 
   getCardPositionInOtherHand(card: OwnedAndUnknownCardInfo): CardPosition {
-    const cardsInHand: OwnedAndUnknownCardInfo[] = this.cards.filter((c: CardInfo) => (c as OwnedAndUnknownCardInfo).ownerId === card.ownerId) as OwnedAndUnknownCardInfo[];
+    const cardsInHand: OwnedAndUnknownCardInfo[] = (this.cards
+      .filter((c: CardInfo) => {
+        return (c as OwnedAndUnknownCardInfo).ownerId === card.ownerId
+          && (c as PlayedCardInfo).playedOnTurn === undefined;
+      }) as OwnedAndUnknownCardInfo[])
+      .sort((cA: OwnedAndUnknownCardInfo, cB: OwnedAndUnknownCardInfo) => cA.id.localeCompare(cB.id));
 
-    return {};
+    const playerPos = this.getPlayerRoundDetailsPosition(card.ownerId);
+    const maxArc = 90;
+    const maxArcStep = maxArc / (cardsInHand.length - 1);
+    const radius = (playerPos.width / 2 + playerPos.height / 2) / 2;
+    const spreadDistance = 15;
+    const angleStep = Math.min(CardContainerComponent.getAngle(spreadDistance, radius), maxArcStep);
+    const angle = (cardsInHand.indexOf(card) - ((cardsInHand.length - 1) / 2)) * angleStep + playerPos.angle + 180;
+
+    return {
+      x: playerPos.x + Math.cos(CardContainerComponent.toRadians(angle - 90)) * playerPos.width / 2,
+      y: playerPos.y + Math.sin(CardContainerComponent.toRadians(angle - 90)) * playerPos.height / 2,
+      z: cardsInHand.indexOf(card),
+      height: 100,
+      rotation: angle
+    };
+  }
+
+  getUnownedCardPosition(): CardPosition {
+    const padding = 20;
+    const cardHeight = 150;
+    const cardWidth = cardHeight / 7 * 5;
+    return {
+      x: this.containerWidth - cardWidth / 2 - padding,
+      y: this.containerHeight - cardHeight / 2 - padding,
+      width: cardWidth,
+      height: cardHeight
+    };
+  }
+
+  getWonCardPosition(card: WonCardInfo): CardPosition {
+    const playerPos = this.getPlayerRoundDetailsPosition(card.winnerId);
+    return {
+      x: playerPos.x,
+      y: playerPos.y,
+      z: 0,
+      height: 50
+    };
+  }
+
+  getPlayedCardPosition(card: PlayedCardInfo): CardPosition {
+    const playerPos = this.getPlayerRoundDetailsPosition(card.ownerId);
+    const rx = this.containerWidth / 5;
+    const ry = this.containerHeight / 5;
+    return {
+      x: Math.cos(CardContainerComponent.toRadians(playerPos.angle - 90)) * rx,
+      y: Math.sin(CardContainerComponent.toRadians(playerPos.angle - 90)) * ry,
+      z: card.playedOrderPosition,
+      height: 125,
+      rotation: playerPos.angle
+    };
   }
 }

7138fa4539bca571fe87bb3829a4cbfca58c8c9a
Thomas Hunter <tommychasseur@gmail.com>
2022-08-31 23:55:14 +0100

    Setup inquirer

--- apps/splatnet-cli/src/main.ts
+++ apps/splatnet-cli/src/main.ts
@@ -1,2 +1,38 @@
-console.log('Test');
-process.exit();
+import { type PromptModule } from 'inquirer';
+
+let prompt: PromptModule = null;
+const getInquirerPrompt = async (): Promise<PromptModule> => {
+  if (prompt !== null) { return prompt; }
+  const module = await (eval(`import('inquirer')`) as Promise<typeof import('inquirer')>);
+  prompt = module.createPromptModule();
+  return prompt;
+};
+
+(async () => {
+  await getInquirerPrompt();
+
+  let continueApp = true;
+  while (continueApp) {
+    const chosenCommand = await prompt([{
+      type: 'list',
+      name: 'command',
+      choices: [
+        {
+          name: 'Generate iksm cookie',
+          value: 'iksm'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          name: 'Exit',
+          value: 'exit'
+        }
+      ]
+    }]);
+    switch (chosenCommand.command) {
+      case 'iksm': console.log('iksm'); break;
+      default: continueApp = false;
+    }
+  }
+})();

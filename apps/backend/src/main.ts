ad769ed842eb03b8d257cf888e1d411b19cf8fd0
Thomas Hunter <tommychasseur@gmail.com>
2022-10-18 21:17:59 +0100

    Generate package.json on backend build + add certificates to Nestjs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,9 +9,17 @@ import { config as dotenvConfig } from 'dotenv';
 
 import { AppModule } from './app/app.module';
 
+import { environment } from './environments/environment';
+import * as fs from 'fs'
+
 async function bootstrap() {
+  const httpsOptions = environment.production ? {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem')
+  } : {};
+
   dotenvConfig();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;

5e324d812484a24a656f35ea58d91864c69f0f69
Thomas Hunter <thomas@thunter.uk>
2025-10-31 16:57:35 +0000

    Remove db connection so it doesn't crash the whole backend when it fails

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { environment } from '../environments/environment';
-import { Connection as MongooseConnection } from 'mongoose';
+// import { Connection as MongooseConnection } from 'mongoose';
 
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { OuistitiModule } from '@TomikaArome/ouistiti';
-import { PostsModule, TagsModule } from '@TomikaArome/common-backend';
+// import { PostsModule, TagsModule } from '@TomikaArome/common-backend';
 
 import { AppController } from './app.controller';
 
@@ -13,24 +13,24 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     OuistitiModule,
-    MongooseModule.forRoot(environment.mongodbConnectionString, {
-      dbName: environment.mongodbDatabase,
-      connectionFactory: (connection: MongooseConnection) => {
-        // Enable validation for updating in MongoDB by default
-        function setRunValidators () {
-          this.setOptions({ runValidators: true });
-        }
-
-        connection.plugin(schema => {
-          schema.pre('findOneAndUpdate', setRunValidators);
-          schema.pre('updateMany', setRunValidators);
-          schema.pre('updateOne', setRunValidators);
-        });
-        return connection;
-      }
-    }),
-    TagsModule,
-    PostsModule
+    // MongooseModule.forRoot(environment.mongodbConnectionString, {
+    //   dbName: environment.mongodbDatabase,
+    //   connectionFactory: (connection: MongooseConnection) => {
+    //     // Enable validation for updating in MongoDB by default
+    //     function setRunValidators () {
+    //       this.setOptions({ runValidators: true });
+    //     }
+    //
+    //     connection.plugin(schema => {
+    //       schema.pre('findOneAndUpdate', setRunValidators);
+    //       schema.pre('updateMany', setRunValidators);
+    //       schema.pre('updateOne', setRunValidators);
+    //     });
+    //     return connection;
+    //   }
+    // }),
+    // TagsModule,
+    // PostsModule
   ],
   controllers: [AppController],
   providers: [
